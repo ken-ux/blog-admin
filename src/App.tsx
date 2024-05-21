c412af0f7fa362097cb2f426554b6cb571386d16
Kenny Nguyen <sokenns@gmail.com>
2024-05-21 14:34:28 -0700

    Make form into component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
+import LoginForm from "./components/LoginForm";
+
 function App() {
   return (
     <div className="border border-red-400">
-      <form action={import.meta.env.VITE_API_URL + "/login"} method="post">
-        <div>
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            id="username"
-            className="border"
-            name="username"
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            className="border"
-            name="password"
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+      <LoginForm />
     </div>
   );
 }
---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,27 @@
+export default function LoginForm() {
+  return (
+    <form action={import.meta.env.VITE_API_URL + "/login"} method="post">
+      <div>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          className="border"
+          name="username"
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          className="border"
+          name="password"
+          required
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
