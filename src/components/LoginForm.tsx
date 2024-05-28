feebd7f5030a546c4bdf37dfb4f5c7fbbec7934f
Kenny Nguyen <sokenns@gmail.com>
2024-05-28 01:15:42 -0700

    Add submit handler function

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,7 +1,44 @@
+import { useState } from "react";
+const url = import.meta.env.VITE_API_URL + "/login";
+
 export default function LoginForm() {
+  const [formData, setFormData] = useState({ username: "", password: "" });
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  async function submit(e: React.FormEvent) {
+    e.preventDefault();
+    
+    const postData = new FormData();
+    postData.append("username", formData.username);
+    postData.append("password", formData.password);
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: postData,
+      });
+
+      const result = await response.json();
+      console.log("Success:", result);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
   return (
     <form
-      action={import.meta.env.VITE_API_URL + "/login"}
+      onSubmit={submit}
+      // action={url}
       method="post"
       className="border min-h-80 p-4 flex flex-col gap-4"
     >
@@ -13,6 +50,8 @@ export default function LoginForm() {
           className="border"
           name="username"
           required
+          value={formData.username}
+          onChange={handleInputChange}
         />
       </div>
       <div className="flex gap-2 justify-between">
@@ -23,6 +62,8 @@ export default function LoginForm() {
           className="border"
           name="password"
           required
+          value={formData.password}
+          onChange={handleInputChange}
         />
       </div>
       <button type="submit" className="border">
