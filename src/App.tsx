6a52463c842a158e52ed6d2ea1d73c2508f56e9a
Kenny Nguyen <sokenns@gmail.com>
2024-05-29 15:22:39 -0700

    Refactor page rendering logic

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,11 @@ function App() {
 
   return (
     <div className="border border-red-400 flex flex-col items-center gap-6 p-4 max-w-screen-xl m-auto my-8">
-      {!authenticated && <LoginPage setAuthenticated={setAuthenticated} />}
-      {authenticated && <EditorPage setAuthenticated={setAuthenticated} />}
+      {!authenticated ? (
+        <EditorPage setAuthenticated={setAuthenticated} />
+      ) : (
+        <LoginPage setAuthenticated={setAuthenticated} />
+      )}
     </div>
   );
 }
