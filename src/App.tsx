ba350dcb120393badd200ece7bc4e624e7c95787
Kenny Nguyen <sokenns@gmail.com>
2024-05-29 15:46:28 -0700

    Style app and editor

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const [authenticated, setAuthenticated] = useState(false);
 
   return (
-    <div className="border border-red-400 flex flex-col items-center gap-6 p-4 max-w-screen-xl m-auto my-8">
+    <div className="border border-red-400 p-4 max-w-screen-xl my-8">
       {authenticated ? (
         <EditorPage setAuthenticated={setAuthenticated} />
       ) : (
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,3 +1,9 @@
 export default function Editor() {
-  return <div>Editor</div>;
+  return (
+    <div className="self-start">
+      <button type="button" className="bg-sky-600 text-white rounded px-2 py-1">
+        + Create New Post
+      </button>
+    </div>
+  );
 }
