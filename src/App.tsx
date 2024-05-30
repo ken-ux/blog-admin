9eea85e75e7e7b62c7bfc183f28d822d7334da25
Kenny Nguyen <sokenns@gmail.com>
2024-05-30 13:58:31 -0700

    Add auto margins to center page content

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const [authenticated, setAuthenticated] = useState(false);
 
   return (
-    <div className="border border-red-400 p-4 max-w-screen-xl my-8">
+    <div className="border border-red-400 p-4 max-w-screen-xl my-8 m-auto">
       {authenticated ? (
         <EditorPage setAuthenticated={setAuthenticated} />
       ) : (
