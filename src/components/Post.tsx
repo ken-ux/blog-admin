dbac2b93fcdccd8c251d97676eaad3a6159881cf
Kenny Nguyen <sokenns@gmail.com>
2024-05-30 02:49:31 -0700

    Use form element for post content

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -8,14 +8,51 @@ export default function Post({
   published,
 }: PostProps) {
   return (
-    <div className="border border-red-500">
-      <p>Title: {title}</p>
-      <p>Text: {text}</p>
-      <p>Posted: {timestamp}</p>
+    <form className="border border-red-500">
+      <div>
+        <label htmlFor="title">Title:</label>
+        <input type="text" id="title" name="title" value={title} readOnly />
+      </div>
+      <div>
+        <label htmlFor="text">Text:</label>
+        <input type="text" id="text" name="text" value={text} readOnly />
+      </div>
+      <div>
+        <label htmlFor="timestamp">Timestamp</label>
+        <input
+          type="date"
+          id="timestamp"
+          name="timestamp"
+          value={timestamp}
+          readOnly
+        />
+      </div>
+      <div>
+        <legend>Published:</legend>
+        <label htmlFor="published_true">True:</label>
+        <input
+          type="radio"
+          id="published_true"
+          name="published"
+          value="true"
+          checked={published ? true : false}
+          readOnly
+        />
+        <label htmlFor="published_false">False:</label>
+        <input
+          type="radio"
+          id="published_false"
+          name="published"
+          value="false"
+          checked={!published ? true : false}
+          readOnly
+        />
+      </div>
+
       <p>Published: {published.toString()}</p>
       <p>{id}</p>
       <button type="button">Edit</button>
       <button type="button">Delete</button>
-    </div>
+    </form>
   );
 }
