61207717db65421b53e1e9f37eb84edaa969e857
Kenny Nguyen <sokenns@gmail.com>
2024-05-30 02:22:07 -0700

    Add post type

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,12 @@
 export interface LoginFormProps {
   setAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
 }
+
+export interface PostProps {
+  title: string;
+  text: string;
+  timestamp: string;
+  id: string | undefined;
+  _id?: string;
+  published: boolean;
+}
