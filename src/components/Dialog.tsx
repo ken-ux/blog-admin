e6317e25e13b2e68c11cb16111a2803da0563d41
Kenny Nguyen <sokenns@gmail.com>
2024-05-31 14:34:40 -0700

    Handle changes in dialog form

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,10 +1,42 @@
+import { useState } from "react";
+import { DialogFormData } from "../types";
+
 export default function Dialog({ clickHandler }: { clickHandler: () => void }) {
+  const [formData, setFormData] = useState<DialogFormData>({
+    title: "",
+    text: "",
+    timestamp: new Date(),
+    published: false,
+  });
+
+  const handleChange = (
+    property: string,
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const newFormData = formData;
+    switch (property) {
+      case "title":
+      case "text":
+        newFormData[property] = e.target.value;
+        break;
+      case "timestamp":
+        newFormData[property] = new Date(e.target.value);
+        break;
+      case "published":
+        newFormData[property] = e.target.value === "true";
+    }
+    setFormData(newFormData);
+    console.log(formData);
+  };
+
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    clickHandler();
+  };
+
   return (
     <dialog className="shadow-xl rounded p-4 min-w-96">
-      <form
-        onSubmit={(e) => e.preventDefault()}
-        className="flex flex-col gap-4"
-      >
+      <form onSubmit={submitHandler} className="flex flex-col gap-4">
         <div className="flex flex-col">
           <label htmlFor="title">Title:</label>
           <input
@@ -12,11 +44,19 @@ export default function Dialog({ clickHandler }: { clickHandler: () => void }) {
             id="title"
             name="title"
             className="border px-2 py-1"
+            required
+            onChange={(e) => handleChange("title", e)}
           />
         </div>
         <div className="flex flex-col">
           <label htmlFor="text">Text:</label>
-          <textarea id="text" name="text" className="border px-2 py-1" />
+          <textarea
+            id="text"
+            name="text"
+            className="border px-2 py-1"
+            required
+            onChange={(e) => handleChange("text", e)}
+          />
         </div>
         <div className="flex flex-col">
           <label htmlFor="timestamp">Timestamp:</label>
@@ -25,6 +65,8 @@ export default function Dialog({ clickHandler }: { clickHandler: () => void }) {
             id="timestamp"
             name="timestamp"
             className="border px-2 py-1"
+            required
+            onChange={(e) => handleChange("timestamp", e)}
           />
         </div>
         <div className="flex gap-2 items-center">
@@ -35,6 +77,8 @@ export default function Dialog({ clickHandler }: { clickHandler: () => void }) {
             name="published"
             value="true"
             className="border"
+            required
+            onChange={(e) => handleChange("published", e)}
           />
           <label htmlFor="published_true">True</label>
           <input
@@ -43,19 +87,21 @@ export default function Dialog({ clickHandler }: { clickHandler: () => void }) {
             name="published"
             value="false"
             className="border"
+            onChange={(e) => handleChange("published", e)}
           />
           <label htmlFor="published_false">False</label>
         </div>
 
-        <div className="self-end">
+        <div className="self-end flex gap-1">
           <button
+            type="button"
             onClick={clickHandler}
             className="bg-slate-600 text-white px-2 py-1 rounded"
           >
             Cancel
           </button>
           <button
-            onClick={clickHandler}
+            type="submit"
             className="bg-sky-600 text-white px-2 py-1 rounded"
           >
             Submit
