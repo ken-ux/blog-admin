c251466f6964fcbea9f1ef8d77392e2ae7152509
Kenny Nguyen <sokenns@gmail.com>
2024-05-31 03:03:40 -0700

    Add dialog and buttons for opening it

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
+import Dialog from "./Dialog";
 import PostContainer from "./PostContainer";
 
 export default function Editor() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const clickHandler = () => {
+    const dialog = document.querySelector("dialog");
+    if (isOpen) {
+      setIsOpen(false);
+      dialog?.close();
+    } else {
+      setIsOpen(true);
+      dialog?.showModal();
+    }
+  };
+
   return (
     <div>
-      <button type="button" className="bg-sky-600 text-white rounded px-2 py-1 mb-4">
+      <Dialog clickHandler={clickHandler} />
+      <button
+        type="button"
+        onClick={clickHandler}
+        className="bg-sky-600 text-white rounded px-2 py-1 mb-4"
+      >
         + Create New Post
       </button>
       <PostContainer />
