4e8deac95f14f8fe31d2a0017d3f8d53a5795c19
Kenny Nguyen <sokenns@gmail.com>
2024-05-30 21:38:43 -0700

    Add two column layout for posts

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -27,89 +27,93 @@ export default function Post({
   return (
     <form
       onSubmit={(e) => e.preventDefault()}
-      className="border border-red-500 flex flex-col gap-2 p-6"
+      className="border border-slate-300 rounded-lg flex flex-col md:flex-row gap-8 p-6 shadow-md"
     >
-      <div className="flex flex-col">
-        <label htmlFor="title">Title:</label>
-        <input
-          type="text"
-          id="title"
-          name="title"
-          defaultValue={title}
-          disabled={editable ? false : true}
-          className="border px-2 py-1 w-1/2"
-        />
+      <div className="grow-[2] flex flex-col gap-2">
+        <div className="flex flex-col">
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            defaultValue={title}
+            disabled={editable ? false : true}
+            className="border px-2 py-1 w-full"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="text">Text:</label>
+          <textarea
+            id="text"
+            name="text"
+            defaultValue={text}
+            disabled={editable ? false : true}
+            className="border px-2 py-1 w-full"
+          />
+        </div>
       </div>
-      <div className="flex flex-col">
-        <label htmlFor="text">Text:</label>
-        <textarea
-          id="text"
-          name="text"
-          defaultValue={text}
-          disabled={editable ? false : true}
-          className="border px-2 py-1 w-1/2"
-        />
-      </div>
-      <div className="flex gap-2 items-center">
-        <label htmlFor="timestamp">Timestamp:</label>
-        <input
-          type="date"
-          id="timestamp"
-          name="timestamp"
-          defaultValue={parseDate(new Date(timestamp))}
-          disabled={editable ? false : true}
-          className="border px-2 py-1"
-        />
-      </div>
-      <div className="flex gap-2 items-center">
-        <legend>Published:</legend>
-        <input
-          type="radio"
-          id="published_true"
-          name="published"
-          value="true"
-          defaultChecked={published ? true : false}
-          disabled={editable ? false : true}
-          className="border"
-        />
-        <label htmlFor="published_true">True</label>
-        <input
-          type="radio"
-          id="published_false"
-          name="published"
-          value="false"
-          checked={!published ? true : false}
-          disabled={editable ? false : true}
-          className="border"
-        />
-        <label htmlFor="published_false">False</label>
-      </div>
+      <div className="flex-auto flex flex-col gap-2">
+        <div className="flex flex-col">
+          <label htmlFor="timestamp">Timestamp:</label>
+          <input
+            type="date"
+            id="timestamp"
+            name="timestamp"
+            defaultValue={parseDate(new Date(timestamp))}
+            disabled={editable ? false : true}
+            className="border px-2 py-1"
+          />
+        </div>
+        <div className="flex gap-2 items-center">
+          <legend>Published:</legend>
+          <input
+            type="radio"
+            id="published_true"
+            name="published"
+            value="true"
+            defaultChecked={published ? true : false}
+            disabled={editable ? false : true}
+            className="border"
+          />
+          <label htmlFor="published_true">True</label>
+          <input
+            type="radio"
+            id="published_false"
+            name="published"
+            value="false"
+            checked={!published ? true : false}
+            disabled={editable ? false : true}
+            className="border"
+          />
+          <label htmlFor="published_false">False</label>
+        </div>
 
-      <p>Published: {published.toString()}</p>
+        <p>Published: {published.toString()}</p>
 
-      <p>{id}</p>
-      <div className="flex gap-2">
-        <button
-          type="button"
-          className="bg-red-600 text-white rounded px-2 py-1"
-        >
-          Delete
-        </button>
-        <button
-          type="button"
-          className="bg-sky-600 text-white rounded px-2 py-1"
-          onClick={() => setEditable(!editable)}
-        >
-          {editable ? "Cancel" : "Edit"}
-        </button>
-        {editable && (
+        <p>{id}</p>
+        <div className="flex gap-2">
           <button
-            type="submit"
-            className="bg-sky-600 text-white rounded px-2 py-1"
+            type="button"
+            className="bg-red-600 text-white rounded px-2 py-1"
           >
-            Submit
+            Delete
           </button>
-        )}
+          <button
+            type="button"
+            className="bg-sky-600 text-white rounded px-2 py-1"
+            onClick={() => setEditable(!editable)}
+          >
+            {editable ? "Cancel" : "Edit"}
+          </button>
+          {editable && (
+            <button
+              type="submit"
+              className="bg-sky-600 text-white rounded px-2 py-1"
+            >
+              Submit
+            </button>
+          )}
+        </div>
       </div>
     </form>
   );
