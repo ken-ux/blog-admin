89390526c6eb16b4a2ca4d55feb74d5c2f378d81
Kenny Nguyen <sokenns@gmail.com>
2024-06-04 14:36:10 -0700

    Add margin spacing to root element

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const [authenticated, setAuthenticated] = useState(false);
 
   return (
-    <div className="p-16 max-w-screen-xl my-8 m-auto bg-white rounded-lg shadow-xl">
+    <div className="p-16 max-w-screen-xl m-auto bg-white rounded-lg shadow-xl">
       {authenticated ? (
         <EditorPage setAuthenticated={setAuthenticated} />
       ) : (
