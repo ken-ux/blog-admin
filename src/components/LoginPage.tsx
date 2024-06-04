76149d6922b36b532f044c3fd742f844af392368
Kenny Nguyen <sokenns@gmail.com>
2024-06-04 14:23:25 -0700

    Keep user logged in when JWT is still valid

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,7 +1,23 @@
+import { useEffect } from "react";
 import LoginForm from "./LoginForm";
 import { LoginFormProps } from "../types";
 
 export default function LoginPage({ setAuthenticated }: LoginFormProps) {
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    async function auth() {
+      const url = import.meta.env.VITE_API_URL + "/login";
+      const response = await fetch(url, {
+        headers: { Authorization: token as string },
+      });
+      if (response.status === 200) {
+        setAuthenticated(true);
+      }
+    }
+    auth();
+  });
+
   return (
     <main className="flex flex-col items-center gap-6">
       <h1 className="text-2xl">Blog Admin</h1>
